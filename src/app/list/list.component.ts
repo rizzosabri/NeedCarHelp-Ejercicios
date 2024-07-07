7cbe7344becc8972be84197cd2e68c9dbd3e6bca
rizzosabri <sabri.rizzo@gmail.com>
2024-07-07 20:08:31 +0200

    Add deleteList function to remove content from localStorage

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
   @Input() tasks: string[] = [];
+
+  deleteList() {
+    alert("Deseas Eliminar todas las Tareas?")
+    this.tasks = [];
+    localStorage.clear()
+
+  }
 
 }
