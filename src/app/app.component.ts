28545999c1a0a59f2bde76dfd22b2c67aa925049
rizzosabri <sabri.rizzo@gmail.com>
2024-07-07 19:27:41 +0200

    import FormComponent and ListComponent in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
