d3b0d0af05c7f4c98068b3084f7c5804edcf77ef
rizzosabri <sabri.rizzo@gmail.com>
2024-07-07 19:54:56 +0200

    Capture user input data in form component and communicate for task creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,8 @@ export class AppComponent {
   title = 'toDoList-app';
 
   tasks: string[] = [];
+
+  addTask(task: string) {
+    this.tasks.push(task);
+  }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
 
+  
+  @Output() newTask = new EventEmitter<string>();
+  task: string = '';
+
+  saveForm() {
+    this.newTask.emit(this.task);
+    alert('Formulario guardado');
+    this.task = '';
+
+  }
 }
