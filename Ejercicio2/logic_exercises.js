fb328278377d94d0675021952a856ee6c5d4e5e2
rizzosabri <sabri.rizzo@gmail.com>
2024-07-07 23:50:02 +0200

    Added fibonacci exercise

---
+++ Ejercicio2/logic_exercises.js
@@ -0,0 +1,21 @@
+
+
+function fibonacci(n) {
+
+    let fibonacciArray = [];
+    fibonacciArray.push(0, n)
+
+    for (let i = 1; i <= n - 2; i++) {
+        fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i])
+    }
+
+    return fibonacciArray;
+}
+
+
+
+
+
+
+
+
