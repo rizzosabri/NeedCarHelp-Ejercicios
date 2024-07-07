9e5d508be7e0391110e555c8c197a72bbb55201e
rizzosabri <sabri.rizzo@gmail.com>
2024-07-07 23:53:19 +0200

    Added prime_numbers

--- Ejercicio2/logic_exercises.js
+++ Ejercicio2/logic_exercises.js
@@ -23,3 +23,50 @@ function isPalindrome(str) {
 
 }
 
+function isPrime_numbers(n) {
+    // Los números menores o iguales a 1 no son primos
+    if (n <= 1) {
+        return false;
+    }
+
+    // Los números 2 y 3 son primos
+    if (n <= 3) {
+        return true;
+    }
+
+    // Los números divisibles por 2 o 3 no son primos
+    if (n % 2 === 0 || n % 3 === 0) {
+        return false;
+    }
+
+    // Verificar divisibilidad desde 5 hasta la raíz cuadrada del número
+    let i = 5;
+    while (i * i <= n) {
+        if (n % i === 0 || n % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+
+}
+
+function prime_numbers(n) {
+
+    let result = [];
+
+    for (let i = 1; i <= n; i++) {
+
+        if (isPrime_numbers(i)) {
+            result.push(i);
+
+        }
+    }
+
+    return result
+
+}
+
+
+
+
